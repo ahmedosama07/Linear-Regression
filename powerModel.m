92249213e2e9be63a34bc0ba9c62fca659b0d436
Ahmed Osama <ahmed.osama8282@gmail.com>
2023-05-24 00:55:18 +0300

    Update powerModel.m

--- powerModel.m
+++ powerModel.m
@@ -3,6 +3,10 @@ function [f, r] = powerModel(y, x)
 %   function calculates constants for power model in least squares method
 Y = log10(y);
 X = log10(x);
+X(X==-Inf) = -10000;
+X(X==Inf) = 10000;
+Y(Y==-Inf) = -10000;
+Y(Y==Inf) = 10000;
 [a0, a1] = leastSquare(Y, X);
 a = 10 .^ a0;
 b = a1;
