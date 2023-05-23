a90ffd51f2c292b1e3955b6c3c2bb563bbce97c9
Ahmed Osama <ahmed.osama8282@gmail.com>
2023-05-23 04:00:36 +0300

    Create leastSquare.m

---
+++ leastSquare.m
@@ -0,0 +1,24 @@
+function [a0, a1] = leastSquare(y, x)
+%leastSquare Summary of this function goes here
+%   Detailed explanation goes here
+n = length(x);
+
+sigmaX = 0;
+sigmaX2 = 0;
+sigmaY = 0;
+sigmaXY = 0;
+
+for i = 1 : length(x)
+    sigmaX = sigmaX + x(i);
+    sigmaX2 = sigmaX2 + (x(i) .^ 2);
+    sigmaY = sigmaY + y(i);
+    sigmaXY = sigmaXY + (x(i) .* y(i));
+end
+
+A = [n sigmaX; sigmaX sigmaX2];
+B = [sigmaY; sigmaXY];
+
+s = linsolve(A, B);
+[a0, a1] = struct('x', num2cell(s)).x;
+end
+
