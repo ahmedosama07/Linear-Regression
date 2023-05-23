e1d3d5aae2697470ce45922bcb4fe3409a105f90
Ahmed Osama <ahmed.osama8282@gmail.com>
2023-05-24 00:55:12 +0300

    Update growthRateModel.m

--- growthRateModel.m
+++ growthRateModel.m
@@ -3,6 +3,10 @@ function [f, r] = growthRateModel(y, x)
 %   function calculates constants for growth rate model in least squares method
 Y = 1 ./ y;
 X = 1 ./ x;
+X(X==-Inf) = -10000;
+X(X==Inf) = 10000;
+Y(Y==-Inf) = -10000;
+Y(Y==Inf) = 10000;
 [a0, a1] = leastSquare(Y, X);
 a = 1 ./ a0;
 b = a1 .* a;
