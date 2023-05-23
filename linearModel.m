248386cef2d54b6bcec3a672006e600260066578
Ahmed Osama <ahmed.osama8282@gmail.com>
2023-05-24 00:55:15 +0300

    Update linearModel.m

--- linearModel.m
+++ linearModel.m
@@ -3,6 +3,10 @@ function [f, r] = linearModel(y, x)
 %   function calculates constants for linear model in least squares method
 X = x;
 Y = y;
+X(X==-Inf) = -10000;
+X(X==Inf) = 10000;
+Y(Y==-Inf) = -10000;
+Y(Y==Inf) = 10000;
 [a0, a1] = leastSquare(Y, X);
 f = a0 + a1 .* x;
 [r, ~, ~] = bestFit(a0, a1, X, Y);
