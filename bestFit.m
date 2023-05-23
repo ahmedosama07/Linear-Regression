b7948111513d62f6864ae43dce7d81337f344e37
Ahmed Osama <ahmed.osama8282@gmail.com>
2023-05-24 00:24:31 +0300

    Update bestFit.m

--- bestFit.m
+++ bestFit.m
@@ -5,17 +5,17 @@ sr = 0;
 st = 0;
 sigmaY = 0;
 n = length(x);
-for i = 1 : length(x)
+for i = 1 : length(y)
     sigmaY = sigmaY + y(i);
 end
 
 yMean = sigmaY ./ n;
 
-for i = 1 : length(x)
-    sr = sr + (y(i) -a0- a1 .* x(i)).^2;
-    st = st + (y(i) - yMean).^2;
+for i = 1 : length(y)
+    sr = sr + (y(i) - a0 - a1 * x(i)) .^ 2;
+    st = st + (y(i) - yMean) .^ 2;
 end
 
-r2=1-sr./st;
-r=sqrt(r2);
+r2 = (st - sr) ./ st;
+r = sqrt(r2);
 end
